1ef682bf593d690503166c31467fbc030a65ad12
SURAJ SINGH RANA <100834595+surajrana45@users.noreply.github.com>
2022-08-08 11:01:32 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (31.34%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    int helper(int*arr,int n)
+    {
+        
+        if(n<=1)
+            return 1;
+        
+        if(arr[n] != -1)
+            return arr[n];
+        
+        int a = helper(arr,n-1);
+        int b = helper(arr,n-2);
+        
+        arr[n] = a+b;
+        return arr[n];
+    }
+    
+    
+    int climbStairs(int n) {
+        
+        int*arr  = new int[n+1];
+        for(int i=0;i<=n;i++)
+        {
+            arr[i] = -1;
+        }
+        
+        int ans = helper(arr,n);
+        
+        return ans;
+        
+    }
+};
