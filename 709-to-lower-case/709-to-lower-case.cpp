69b956a67b6f15a9661ffde8e2845c5168b3f9ac
SURAJ SINGH RANA <100834595+surajrana45@users.noreply.github.com>
2022-08-14 11:05:40 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (54.31%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -2,13 +2,7 @@ class Solution {
 public:
     string toLowerCase(string s) {
         
-        for(int i=0;i<s.length();i++)
-        {
-            if(s[i]>='A' && s[i]<='Z')
-            {
-                s[i] = s[i]+32;
-            }
-        }
+        transform(s.begin(),s.end(),s.begin(),::tolower);
         return s;
     }
 };
