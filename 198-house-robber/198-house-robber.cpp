09b91266dc633cca04e672673f9f108f7e394b45
SURAJ SINGH RANA <100834595+surajrana45@users.noreply.github.com>
2022-08-17 15:02:45 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (25.38%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,32 +1,48 @@
 class Solution {
 public:
     
-    int helper(vector<int>v,int n ,int*arr)
-    {
-      if(n<0)
-          return 0;
-      if(n==0)
-          return v[n];
-      if(n == 1)
-          return max(v[n],v[n-1]);
+//     int helper(vector<int>v,int n ,int*arr)
+//     {
+      // if(n<0)
+      //     return 0;
+      // if(n==0)
+      //     return v[n];
+//       if(n == 1)
+//           return max(v[n],v[n-1]);
         
-        if(arr[n] != -1)
-            return arr[n];
+//         if(arr[n] != -1)
+//             return arr[n];
         
-       arr[n] =  max(helper(v,n-1,arr),v[n]+helper(v,n-2,arr)); 
-        return arr[n];   
+//        arr[n] =  max(helper(v,n-1,arr),v[n]+helper(v,n-2,arr)); 
+//         return arr[n];   
         
-    }
+//     }
     
     int rob(vector<int>& nums) {
         
+       int n = nums.size()-1 ;
        int*arr = new int[nums.size()];
         for(int i=0;i<nums.size();i++)
         {
             arr[i] = -1;
         }
-      int ans = helper(nums,nums.size()-1,arr);
-        return ans;
+        
+       if(n<0)
+          return 0;
+       if(n==0)
+          return nums[n];
+        
+        arr[0] = nums[0];
+        arr[1] = max(nums[0],nums[1]);
+        int i;
+        for(i=2;i<nums.size();i++)
+        {
+            arr[i] = max(arr[i-1],nums[i]+arr[i-2]);
+        }
+        return arr[i-1]; 
+        
+      // int ans = helper(nums,nums.size()-1,arr);
+      //   return ans;
         
     }
     
