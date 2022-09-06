184462c51380b4fbc11d008d8d953dc9e322338b
SURAJ SINGH RANA <100834595+surajrana45@users.noreply.github.com>
2022-09-06 22:04:20 +0530

    Time: 26 ms (50.17%), Space: 13.7 MB (44.41%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,45 +1,41 @@
 class Solution {
 public:
     
-   static bool cmp(pair<int,int>&a , pair<int,int>&b)
+    static bool cmp(pair<int,int>a,pair<int,int>b)
     {
         return a.second>b.second;
     }
     
-    vector<int> sortmap(map<int,int>&m,int k)
-    {
-        vector<pair<int,int>>v;
-        
-        for(auto it:m)
-        {
-            v.push_back(it);
-        }
-        
-        sort(v.begin(),v.end(),cmp);
-        int i=0;
-        vector<int>v1;
-        for(auto it:v)
-        {
-            v1.push_back(it.first);
-            i++;
-            if(i==k)
-                break;
-        }
-        
-        return v1;
-    }
     
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
-        map<int,int>m;
+        unordered_map<int,int>mp;
         
         for(int i=0;i<nums.size();i++)
         {
-            m[nums[i]]++;
+            mp[nums[i]]++;
+        }
+        
+        vector<pair<int,int>>v;
+        
+        for(auto it:mp)
+        {
+            v.push_back({it.first,it.second});
+        }
+        
+        sort(v.begin(),v.end(),cmp);
+        
+        vector<int>ans;
+        int i=1;
+        for(auto it:v)
+        {
+            ans.push_back(it.first);
+           i++;
+            if(i > k)
+                break;
         }
         
         
-        vector<int>v = sortmap(m,k);
-     return v;      
+      return ans;
     }
 };
