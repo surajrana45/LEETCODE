157bd27e7eacb712426260e37c7495531a69c932
SURAJ SINGH RANA <100834595+surajrana45@users.noreply.github.com>
2022-09-17 11:58:11 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (54.14%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    
+    string getS(int d)
+    {
+        if(d == 2)
+            return "abc";
+        if(d == 3)
+            return "def";
+        if(d == 4)
+            return "ghi";
+        if(d == 5)
+            return "jkl";
+        if(d == 6)
+            return "mno";
+        if(d == 7)
+            return "pqrs";
+        if(d == 8)
+            return "tuv";
+        if(d == 9)
+            return "wxyz";
+        return " ";
+    }
+    
+    
+    void helper(vector<string>&ans,int n)
+    {
+        if(n == 0)
+        {
+            ans.push_back("");
+            return;
+        }
+        int r = n%10;
+        n = n/10;
+        helper(ans,n);
+        string str = getS(r);
+        int l = ans.size();
+        for(int i=0;i<str.length()-1;i++)
+        {
+            for(int j=0;j<l;j++)
+                ans.push_back(ans[j]);
+        }
+        int k =0 ;
+        for(int i = 0;i<str.length();i++)
+        {
+            for(int j=0;j<l;j++)
+            {
+                ans[k].push_back(str[i]);
+                k++;
+            }
+        }
+    }
+    
+    
+    vector<string> letterCombinations(string digits) {
+        if(digits == "")
+            return {};
+        int n = stoi(digits);
+       // int l = digits.length();
+        vector<string>ans;
+        helper(ans,n);
+        return ans;
+    }
+};
