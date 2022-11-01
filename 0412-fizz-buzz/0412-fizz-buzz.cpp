ea855d34de1ec176363fcc296ce2ba31178c27fd
SURAJ SINGH RANA <100834595+surajrana45@users.noreply.github.com>
2022-11-01 22:43:05 +0530

    Time: 8 ms (41.26%), Space: 7.3 MB (88.17%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        
+        vector<string>ans(n);
+        for(int i=0;i<n;i++)
+        {
+            if((i+1)%3==0 && (i+1)%5==0)
+            {
+                ans[i] = "FizzBuzz";
+            }
+            else if((i+1)%3==0)
+            {
+                ans[i]="Fizz";
+            }
+            else if((i+1)%5==0)
+            {
+                ans[i] = "Buzz";
+            }
+            else
+            {
+                string t = to_string(i+1);
+                ans[i]=t;
+            }
+        }
+        return ans;
+    }
+};
